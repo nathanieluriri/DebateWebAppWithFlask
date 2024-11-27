4e56c5bef7865b15aaeb2d545e36d3e42052e077
Joshua Ajorgbor <joshuaajorgbor@gmail.com>
2024-11-27 20:28:08 +0100

    implemented base frontend structure

--- app.py
+++ app.py
@@ -3,14 +3,17 @@ from flask import Flask, render_template
 # Create a Flask app instance
 app = Flask(__name__)
 
-# Define routes directly
-@app.route('/')
-def home():
-    return render_template('index.html')
 
-@app.route('/about')
+# Define routes directly
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template("about.html")
+
 
 # Run the app
 if __name__ == "__main__":
